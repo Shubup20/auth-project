65efcf1bef9e777f04b16df03ffd75851c4ac18e
SHUBHAM CHAUHAN <shubhamchauhanpoly@gmail.com>
2025-05-06 15:45:28 +0530

    export default module

--- models/postModel.js
+++ models/postModel.js
@@ -1,9 +1,21 @@
 import mongoose from "mongoose";
 
-const postSchema = mongoose.model({
-  title:{
-    type:string,
-    required:[true,"title is required!"],
-    trime:true,
+const postSchema = mongoose.model(
+  {
+    title: {
+      type: string,
+      required: [true, "title is required!"],
+      trime: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
   }
-})
+);
+
+export default mongoose.model("Post", postSchema);
