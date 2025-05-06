23bb3e874c69e9176c6d9147b2e90ff590e23f40
SHUBHAM CHAUHAN <shubhamchauhanpoly@gmail.com>
2025-05-06 15:31:55 +0530

    mongodb connected and create userModel userSchema

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
 
+// Initializes your Express application.
 const app = express();
 
+//middleware
+app.use(cors());
+app.use(helmet());
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("DB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//routes
 app.get("/", (req, res) => {
   res.json({ message: "hello from server" });
 });
 
-app.listen(process.env.PORT,()=>{
-  console.log('listening....');
+//start server
+app.listen(process.env.PORT, () => {
+  console.log("listening....");
 });
---
+++ models/usersModel.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const userScchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required!"],
+      trim: true,
+      unique: [true, "Email must be unique!"],
+      minlength: [5, "Email must be 5 characters!"],
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password must be provided"],
+      trim: true,
+      select: false,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationCode: {
+      type: String,
+      select: false,
+    },
+    verificationCodeValidation: {
+      type: Number,
+      select: false,
+    },
+    forgotPasswordCode: {
+      type: String,
+      select: false,
+    },
+    forgotPasswordCodeValidation: {
+      type: Number,
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userScchema);
